1bf19aacd3c0c9e0d54d6ebb35c727c78bc2ec6e
Saeed Badran <salouri@gmail.com>
2023-09-14 08:47:53 +0300

    cart-service: add cart client inside shopper service

---
+++ shopper-service/server/services/CartServiceClient.js
@@ -0,0 +1,90 @@
+const config = require("../config");
+const ServiceClient = require("./ServiceClient");
+/** @moduleCartClient */
+/**
+ * client class for interacting with the  Cart service
+ */
+class CartServiceClient {
+  static key(userId) {
+    return `shopper_cart:${userId}`;
+  }
+
+  static client() {
+    return config.redis.client;
+  }
+
+  /**
+   * Get all items from the database, sorted in descending order by creation time
+   * @returns {Promise<Array>} - A promise that resolves to an array of Items
+   */
+  static async getAll(userId) {
+    try {
+      const result = await ServiceClient.callService("cart-service", {
+        method: "get",
+        url: `/items/${userId}`
+      });
+      return result;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  /**
+   * Get a single item from the database
+   * @param {string} itemId - The id of the item to retrieve
+   * @returns {Promise<Object>} - A promise that resolves to an Item object
+   */
+  static async getOne(itemId) {
+    try {
+      const result = await ServiceClient.callService("cart-service", {
+        method: "get",
+        url: `/items/${itemId}`
+      });
+      return result;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
+  /**
+   * Add an item to the user's cart
+   * @param {string} itemId - The ID of the item to add
+   * @returns {Promise<number>} - A promise that resolves to the new quantity of
+   * the item in the cart
+   */
+  static async add(userId, itemId) {
+    try {
+      const result = await ServiceClient.callService("cart-service", {
+        method: "post",
+        url: `/items/${userId}`,
+        data: { itemId }
+      });
+      return result;
+    } catch (error) {
+      console.error(error);
+      throw new Error(error);
+    }
+  }
+
+  /**
+   * Remove an item from the database
+   * @param {string} itemId - The id of the item to remove
+   * @returns {Promise<Object>} - A promise that resolves to the deletion result
+   */
+  static async remove(userId, itemId) {
+    try {
+      const result = await ServiceClient.callService("cart-service", {
+        method: "delete",
+        url: `/items/${userId}/${itemId}`
+      });
+      return result;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+}
+
+module.exports = CartServiceClient;
