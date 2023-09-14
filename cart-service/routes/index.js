eda2feaa7f641664f2f0c20157e6edadfe71bd99
Saeed Badran <salouri@gmail.com>
2023-09-14 08:45:00 +0300

    cart-service: routes

--- cart-service/routes/index.js
+++ cart-service/routes/index.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const CartService = require("../lib/CartService");
+
 const router = express.Router();
 
-// Define your RESTful routes here
-router.get("/", (req, res) => {
-  // Return a JSON response with a 'hello world' message
-  res.json({ msg: "hello world" });
+router.get("/items/:userId", async (req, res) => {
+  try {
+    const items = await CartService.getAll(req.params.userId);
+
+    return res.json(items);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("General error");
+  }
 });
 
+router.post("/items/:userId", async (req, res) => {
+  try {
+    await CartService.add(req.params.userId, req.body.itemId);
+    return res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("General error");
+  }
+});
+
+router.delete("/items/:userId/:itemId", async (req, res) => {
+  try {
+    await CartService.remove(req.params.userId, req.params.itemId);
+    return res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("General error");
+  }
+});
 module.exports = router;
